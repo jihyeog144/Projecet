b7a1ab73552f30bf903e7bab56765253451a7a0b
jihyeog144 <jiheog144@gmail.com>
2025-05-30 01:28:00 +0900

    [Fix] Ui & PlayerController
    스크립트 수정 및 확장

--- Assets/Scrpits/PlayerController.cs
+++ Assets/Scrpits/PlayerController.cs
@@ -2,25 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : GameManager
 {
-    public string playerName;
     public bool isAlive = true;
     public int MaxHp = 3;
     public int CurrentHp;
 
+    public Text statusText; // 플레이어 상태를 나타내는 텍스트
+
     public void Die()
     {
         isAlive = false;
-        Debug.Log(playerName + " 이(가) 탈락하였습니다!");
+        Debug.Log("탈락하였습니다!");
         // 탈락 애니메이션, 이펙트 등을 여기에
     }
 
     public void ReactToBlank()
     {
-        Debug.Log(playerName + " 살아남았습니다.. 운이 좋았군.");
-        // 한숨 효과음 등
+        Debug.Log($"휴… 공포탄이었군.");
+        if (statusText != null)
+            statusText.text = "생존!";
     }
 
     public void Hit(ShellType shellType)
--- Assets/Scrpits/UIManager.cs
+++ Assets/Scrpits/UIManager.cs
@@ -1,22 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject shellSlotPrefab;     // 탄환 아이콘 프리팹
-    public Transform shellPanelParent;     // 탄환 리스트 부모 오브젝트
-
+    public GameObject shellSlotPrefab; // 탄환 아이콘 프리팹
+    public Transform shellPanelParent; // 탄환 아이콘들을 담는 부모 오브젝트
     public Sprite blankSprite;
     public Sprite buckshotSprite;
+    public Text shellCountText;
+    public Button fireButton;
 
     private List<GameObject> shellSlots = new();
 
-    // 탄환 리스트 구성
     public void CreateShellUI(List<Shell> shells)
     {
-        // 기존 슬롯 제거
+        // 기존 UI 제거
         foreach (var slot in shellSlots)
             Destroy(slot);
         shellSlots.Clear();
@@ -26,23 +25,27 @@ public class UIManager : MonoBehaviour
             GameObject slot = Instantiate(shellSlotPrefab, shellPanelParent);
             Image img = slot.GetComponent<Image>();
             img.sprite = shell.Type == ShellType.Blank ? blankSprite : buckshotSprite;
-            img.color = new Color(1, 1, 1, 0.3f); // 비활성 상태처럼 처리
+            img.color = new Color(1f, 1f, 1f, 0.2f); // 처음엔 흐리게
             shellSlots.Add(slot);
         }
     }
 
-    // 발사 후 남은 탄 표시 강조
     public void HighlightFiredShell(int index)
     {
         if (index < shellSlots.Count)
         {
             var img = shellSlots[index].GetComponent<Image>();
-            img.color = Color.white; // 사용된 탄 표시
+            img.color = Color.white;
         }
     }
 
-    public void UpdateShellUI()
+    public void UpdateShellUI(int remaining)
     {
+        shellCountText.text = $"남은 탄환: {remaining}";
+    }
 
+    public void EnableFireButton(bool enable)
+    {
+        fireButton.interactable = enable;
     }
 }
