69a7a4ef44239afbeef7d503e567d76ff2e23ba6
jihyeog144 <jiheog144@gmail.com>
2025-06-02 00:34:11 +0900

    [Feat] Scene
    씬 연결 구현

--- Assets/Scrpits/StartManager.cs
+++ Assets/Scrpits/StartManager.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        // 게임시작
+        SceneManager.LoadScene("GameScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        // 게임종료
+        Application.Quit();
     }
 }
